2585dda42d972aa60186c0c944b8971d4497f4c4
Roxime13 <sanrulljar@gmail.com>
2024-05-03 16:41:04 +0200

    modificacions app i començament documentació instal.lació Flask

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Exemple de README
 
+## Instal.lació Flask i entorn
+
+1. Per crear l'entorn de Flask, primer hi ha que seguir les següents passes: 
+ **Windows **                      **Linux**
+ - .prova/Scripts/activate      -Per activar: source .prova/Scripts/activate
+ - Per desactivar: desactivate  -Per desactivar: desactivate
+
+2. Instal.lar pip (python package Index)
+2.1. 
+
 Això és un [enllaç](https://google.com) i això són **negretes** i *cursiva* i ara ve una llista:
 
 * hola
--- app.py
+++ app.py
@@ -85,9 +85,9 @@ def lavanguardia(seccio):
 
 def get_rss_lavanguardia(seccio):
     # MODE REMOT: versió on descarrega l'XML de la web
-    # xml = f"https://www.lavanguardia.com/rss/{seccio}.xml"
+    # xml = f"https://www.lavanguardia.com/rss/{seccio}.xml" #una manera de conectar el xml
     
-    # MODE LOCAL: versió que fa servir l'XML descarregat
+    # MODE LOCAL: versió que fa servir l'XML descarregat 
     xml = f"./rss/lavanguardia/{seccio}.xml"
     
     rss = feedparser.parse(xml)
